975397d47905e4c9b8bfe057fae9048ae3ad9a30
Nattawood <golfmanayo@gmail.com>
2015-03-17 11:17:59 +0700

    1.9

---
+++ src/main/webapp/views/js/languageControllers.js
@@ -0,0 +1,14 @@
+/**
+ * Created by CAMT on 3/17/2015.
+ */
+'use strict';
+var langController = angular.module('languageControllers',['languageServices']);
+
+langController.controller('languageController',['$scope','$translate','$location',
+    function($scope,$translate,$location){
+        $scope.changeLanguage = function(locale){
+            $translate.use(locale);
+            $location.search('lang',locale);
+        }
+    }
+])
