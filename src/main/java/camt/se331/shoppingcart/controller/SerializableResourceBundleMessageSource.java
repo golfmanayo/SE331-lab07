cd40acdf50b61df9da0768235fc789f53176492a
Nattawood <golfmanayo@gmail.com>
2015-03-17 10:20:07 +0700

    1.3

---
+++ src/main/java/camt/se331/shoppingcart/controller/SerializableResourceBundleMessageSource.java
@@ -0,0 +1,19 @@
+package camt.se331.shoppingcart.controller;
+
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+import java.util.Properties;
+
+/**
+ * Created by CAMT on 3/17/2015.
+ */
+public class SerializableResourceBundleMessageSource extends ReloadableResourceBundleMessageSource{
+    public Properties getAllProperties(Locale locale){
+        clearCacheIncludingAncestors();
+        PropertiesHolder propertiesHolder = getMergedProperties(locale);
+        Properties properties = propertiesHolder.getProperties();
+
+        return properties;
+    }
+}
